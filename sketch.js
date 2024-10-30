bc7e6817c8cdf2001433448f40977f5f8a17bfe2
52muffin <myosotishy@duksung.ac.kr>
2024-10-30 10:43:06 +0900

    네 번째 업로드

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function preload() {
 
 function setup() {
   createCanvas(1280, 720);
-  //noStroke();
+  noStroke();
 }
 
 function draw() {
@@ -14,10 +14,11 @@ function draw() {
   if (mouseIsPressed == true) {
     console.log("mouseX: " + mouseX, "mouseY: " + mouseY);
   }
+  fill(0, 150);
   //1
   beginShape();
   vertex(568, 315);
-  vertex(572, 344);
+  vertex(571, 345);
   vertex(617, 335);
   endShape(CLOSE);
   //2
@@ -46,14 +47,123 @@ function draw() {
   endShape(CLOSE);
   //6
   beginShape();
-  vertex(673, 309);
+  vertex(674, 310);
   vertex(609, 274);
   vertex(653, 229);
   endShape(CLOSE);
   //7
   beginShape();
-  vertex(673, 309);
+  vertex(674, 310);
   vertex(609, 274);
   vertex(617, 335);
+  endShape(CLOSE);
+  //8
+  beginShape();
+  vertex(674, 310);
+  vertex(708, 334);
+  vertex(645, 360);
+  endShape(CLOSE);
+  //9
+  beginShape();
+  vertex(674, 310);
+  vertex(708, 334);
+  vertex(696, 271);
+  vertex(653, 229);
+  endShape(CLOSE);
+  //10
+  beginShape();
+  vertex(755, 269);
+  vertex(696, 271);
+  vertex(653, 229);
+  endShape(CLOSE);
+  //11
+  beginShape();
+  vertex(755, 269);
+  vertex(696, 271);
+  vertex(785, 327);
+  endShape(CLOSE);
+  //12
+  beginShape();
+  vertex(708, 334);
+  vertex(696, 271);
+  vertex(785, 327);
+  endShape(CLOSE);
+  //13
+  beginShape();
+  vertex(708, 334);
+  vertex(752, 377);
+  vertex(785, 327);
+  endShape(CLOSE);
+  //14
+  beginShape();
+  vertex(846, 319);
+  vertex(818, 265);
+  vertex(785, 327);
+  endShape(CLOSE);
+  //15
+  beginShape();
+  vertex(755, 269);
+  vertex(818, 265);
+  vertex(785, 327);
+  endShape(CLOSE);
+  //16
+  beginShape();
+  vertex(755, 269);
+  vertex(818, 265);
+  vertex(757, 229);
+  endShape(CLOSE);
+  //17
+  beginShape();
+  vertex(755, 269);
+  vertex(653, 229);
+  vertex(757, 229);
+  endShape(CLOSE);
+  //18
+  beginShape();
+  vertex(676, 163);
+  vertex(653, 229);
+  vertex(757, 229);
+  endShape(CLOSE);
+  //19
+  beginShape();
+  vertex(676, 163);
+  vertex(653, 229);
+  vertex(592, 209);
+  endShape(CLOSE);
+  //20
+  beginShape();
+  vertex(676, 163);
+  vertex(583, 149);
+  vertex(592, 209);
+  endShape(CLOSE);
+  //21
+  beginShape();
+  vertex(676, 163);
+  vertex(583, 149);
+  vertex(671, 118);
+  endShape(CLOSE);
+  //22
+  beginShape();
+  vertex(676, 163);
+  vertex(739, 126);
+  vertex(671, 118);
+  endShape(CLOSE);
+  //23
+  beginShape();
+  vertex(676, 163);
+  vertex(739, 126);
+  vertex(757, 229);
+  endShape(CLOSE);
+  //24
+  beginShape();
+  vertex(825, 215);
+  vertex(739, 126);
+  vertex(757, 229);
+  endShape(CLOSE);
+  //25
+  beginShape();
+  vertex(825, 215);
+  vertex(818, 265);
+  vertex(757, 229);
   endShape(CLOSE);
 }
