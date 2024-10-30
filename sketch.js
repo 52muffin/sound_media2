7d70db10f3919f900bb5130fe9e3dd55c855daed
52muffin <myosotishy@duksung.ac.kr>
2024-10-30 10:42:41 +0900

    세 번째 업로드

--- sketch.js
+++ sketch.js
@@ -6,12 +6,54 @@ function preload() {
 
 function setup() {
   createCanvas(1280, 720);
+  //noStroke();
 }
 
 function draw() {
-  //background(220);
   image(img, 0, 0);
   if (mouseIsPressed == true) {
     console.log("mouseX: " + mouseX, "mouseY: " + mouseY);
   }
+  //1
+  beginShape();
+  vertex(568, 315);
+  vertex(572, 344);
+  vertex(617, 335);
+  endShape(CLOSE);
+  //2
+  beginShape();
+  vertex(568, 315);
+  vertex(609, 274);
+  vertex(617, 335);
+  endShape(CLOSE);
+  //3
+  beginShape();
+  vertex(568, 315);
+  vertex(609, 274);
+  vertex(547, 255);
+  endShape(CLOSE);
+  //4
+  beginShape();
+  vertex(592, 209);
+  vertex(609, 274);
+  vertex(547, 255);
+  endShape(CLOSE);
+  //5
+  beginShape();
+  vertex(592, 209);
+  vertex(609, 274);
+  vertex(653, 229);
+  endShape(CLOSE);
+  //6
+  beginShape();
+  vertex(673, 309);
+  vertex(609, 274);
+  vertex(653, 229);
+  endShape(CLOSE);
+  //7
+  beginShape();
+  vertex(673, 309);
+  vertex(609, 274);
+  vertex(617, 335);
+  endShape(CLOSE);
 }
